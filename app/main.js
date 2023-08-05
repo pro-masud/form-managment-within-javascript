d21dcd6a92bc4b5eff72959a297bc3cbeea0400c
pro-mannan <promasudbd@gmail.com>
2023-08-05 10:16:09 +0600

    form managment validation to javascript

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 // get form data select here 
 
 const formData = document.getElementById("formData");
+const msg = document.querySelector(".msg");
 
 
 // submit event here 
@@ -9,5 +10,16 @@ formData.onsubmit = (e) => {
 
     const form_data = new FormData(e.target);
     const data = Object.fromEntries(form_data);
-    console.log(data);
+    
+
+    // form validation
+    if(!data.name || !data.email || !data.mobile){
+        msg.innerHTML = alertStatus("All Fireld Are Requered !");
+    }else if(!isMobile(data.mobile)){
+      msg.innerHTML = alertStatus("Invalied Your Moblie Number", "danger");
+    }else if(!isEmail(data.email)){
+      msg.innerHTML = alertStatus("Invalied Your Email Number", "danger");
+    }else{
+      msg.innerHTML = alertStatus("Your sucssfully lon in your Account", "info");
+    }
 }
